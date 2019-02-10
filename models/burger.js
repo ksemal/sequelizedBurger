51ef593c4abab6f521992a6c559123b7bc026af1
km <42688915+ksemal@users.noreply.github.com>
2019-02-09 23:35:01 -0500

    added models

--- models/burger.js
+++ models/burger.js
@@ -11,11 +11,7 @@ module.exports = function(sequelize, DataTypes) {
   });
 
   Burger.associate = function(models) {
-    Burger.belongsTo(models.Customer, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
+    Burger.belongsTo(models.Customer, {});
   };
   return Burger;
 };
